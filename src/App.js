7a1006921047b27b585474ca75d890ec95eca965
QuangDo <qdangdo@gmail.com>
2021-10-26 01:04:18 -0500

    Add Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import Navbar from './components/Navbar'
+import styled from 'styled-components'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { useState} from 'react'
+
 function App() {
+  const [showNav, setShowNav] = useState(false)
+
   return (
     <div className="App">
-      <h5>HELLO</h5>
+      <NavHeader>
+        < GiHamburgerMenu onClick={() => setShowNav(!showNav)}/>
+      </NavHeader>
+      <Navbar show={showNav} />
     </div>
   );
 }
 
 export default App;
+
+//Styled Components
+const NavHeader = styled.header`
+  color: #eaecef;
+  padding: 15px 30px;
+  background: #011627;
+  display: flex;
+  height: 40px;
+  font-size: 40px;
+`
