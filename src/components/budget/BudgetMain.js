4255fd91edbd68451ce322cd24ae42f6f244e176
QuangDo <qdangdo@gmail.com>
2021-10-27 21:54:37 -0500

    Move budgetplans to budgetmain.js

--- src/components/budget/BudgetMain.js
+++ src/components/budget/BudgetMain.js
@@ -6,12 +6,30 @@ import EditButton from './EditButton'
 
 const BudgetMain = ({user}) => {
     const [percentages, setPercentages] = useState([])
+    const [budgetPlans, setBudgetPlans] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:9292/budgetplan/${user.id}`)
+        .then(resp => resp.json())
+        .then(data => {
+            setBudgetPlans(data)
+            setPercentages(
+                [...data].map(element => {
+                    return({
+                        category: element.category,
+                        plannedPercentage: element.plannedPercentage,
+                        actualPercentage: element.actualPercentage,
+                    })
+                })
+            )
+        })
+    }, [])
     
     return (
         <div>
             <h5>Available Monthly Budget: ${user.balance}</h5>
-            <EditButton />
-            <CategoryTable user={user} setPercentages={setPercentages}/>
+            {percentages? <EditButton /> : null}
+            {budgetPlans? <CategoryTable user={user} budgetPlans={budgetPlans}/> : null}
             {/* {percentages? <PieChart percentages={percentages}/> : null} */}
             {percentages? <BarChart percentages={percentages}/> : null}
         </div>
--- src/components/budget/CategoryTable.js
+++ src/components/budget/CategoryTable.js
@@ -1,75 +1,47 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import ActualTable from './ActualTable'
 
-const CategoryTable = ({user, setPercentages}) => {
+const CategoryTable = ({budgetPlans}) => {
 
-    const [budgetPlans, setBudgetPlans] = useState([])
-
-    useEffect(() => {
-        fetch(`http://localhost:9292/budgetplan/${user.id}`)
-        .then(resp => resp.json())
-        .then(data => {
-            setBudgetPlans(data)
-            setPercentages(
-                [...data].map(element => {
-                    return({
-                        category: element.category,
-                        plannedPercentage: element.plannedPercentage,
-                        actualPercentage: element.actualPercentage,
-                    })
-                })
-            )
+    const data = [...budgetPlans].map(element => {
+        return({
+            category: element.category,
+            plannedPercentage: (element.plannedPercentage).toFixed(2)+'%',
+            plannedMoney: '$'+(element.plannedMoney).toFixed(2),
+            actualDollar: '$'+(element.actualDollar).toFixed(2),
+            actualPercentage: (element.actualPercentage).toFixed(2)+'%',
         })
-    }, [])
+    })
+    
+    const columns = [
+        {
+            Header: 'Category',
+            accessor: 'category'
+        },
+        {
+            Header: 'Planned %',
+            accessor: 'plannedPercentage'
+        },
+        {
+            Header: 'Planned $',
+            accessor: 'plannedMoney'
+        },
+        {
+            Header: 'Actual %',
+            accessor: 'actualPercentage'
+        },
+        {
+            Header: 'Actual $',
+            accessor: 'actualDollar'
+        },
+    ]
 
-    if (budgetPlans) {
+    return (
+        <div>
+            <ActualTable data={data} columns={columns}/>
+        </div>
+    )
 
-        const data = [...budgetPlans].map(element => {
-            return({
-                category: element.category,
-                plannedPercentage: (element.plannedPercentage).toFixed(2)+'%',
-                plannedMoney: '$'+(element.plannedMoney).toFixed(2),
-                actualDollar: '$'+(element.actualDollar).toFixed(2),
-                actualPercentage: (element.actualPercentage).toFixed(2)+'%',
-            })
-        })
-        
-        const columns = [
-            {
-                Header: 'Category',
-                accessor: 'category'
-            },
-            {
-                Header: 'Planned %',
-                accessor: 'plannedPercentage'
-            },
-            {
-                Header: 'Planned $',
-                accessor: 'plannedMoney'
-            },
-            {
-                Header: 'Actual %',
-                accessor: 'actualPercentage'
-            },
-            {
-                Header: 'Actual $',
-                accessor: 'actualDollar'
-            },
-        ]
-
-        return (
-            <div>
-                <ActualTable data={data} columns={columns}/>
-            </div>
-        )
-    }
-    else {
-        return(
-            <>
-
-            </>
-        )
-    }
 }
 
 export default CategoryTable
